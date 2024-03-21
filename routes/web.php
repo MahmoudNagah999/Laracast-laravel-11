8357d6afd03820b6168d0b2dea77ef9d6ba9ed3f
MahmoudNagah999 <Mahmoudnagah999@gmail.com>
2024-03-21 23:11:19 +0200

    Day 7

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
 
 Route::get('/', function () {
     return view('home');
@@ -17,46 +19,11 @@ Route::get('/contact', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title'=>'Software Engineer',
-                'salary'=>'10,000'
-            ],
-            [
-                'id' =>2 ,
-                'title'=>'Data Scientist',
-                'salary'=>'20,000'
-            ],
-            [
-                'id' =>3 ,
-                'title'=>'Data Engineer',
-                'salary'=>'30,000'
-            ]
-
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title'=>'Software Engineer',
-            'salary'=>'10,000'
-        ],
-        [
-            'id' =>2 ,
-            'title'=>'Data Scientist',
-            'salary'=>'20,000'
-        ],
-        [
-            'id' =>3 ,
-            'title'=>'Data Engineer',
-            'salary'=>'30,000'
-        ]
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+   $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
